6ead1f3f8dc4b8761d2909547057b195f24ef984
FaizaSuraw <itssumeya@gmail.com>
2025-05-19 12:18:59 +0300

    Adding main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+import './style.css';
+
+// Add any JavaScript logic here (optional)
+console.log('Urban Haven script running!');
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/urban-haven-landing-page/',
+  base: '/urban-haven-landing-page', // Match your repo name!
 });
